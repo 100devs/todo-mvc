5f5e26000900646e8a62dc52a6e58e8d01fa72dc
Brandi <brandihollee@gmail.com>
2022-08-23 15:37:21 -0400

    commented database js

--- config/database.js
+++ config/database.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose') // require mongoose 
 
-const connectDB = async () => {
+const connectDB = async () => { //promise syntax
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
+    const conn = await mongoose.connect(process.env.DB_STRING, { // conn using mongoose connect using a variable found in .env (the place of secrets) called DB_STRING
+      useNewUrlParser: true, // a parser to help with URL passed to JavaScript
+      useUnifiedTopology: true, // something about how MongoDB connects
+      useFindAndModify: false, // do not allow find and modify for it is depreciated
     })
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
-  } catch (err) {
-    console.error(err)
-    process.exit(1)
+    console.log(`MongoDB Connected: ${conn.connection.host}`) // tell us that mongodb is connected
+  } catch (err) { // iff there is an error
+    console.error(err) // tell me what the error is
+    process.exit(1) // and stop this process 
   }
 }
 
-module.exports = connectDB
+module.exports = connectDB // be able to export this to other files using connectDB as a variable name
