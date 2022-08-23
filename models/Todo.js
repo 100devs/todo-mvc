f5f3fd789f507ee6cdc8244f67d72fe63896e905
Brandi <brandihollee@gmail.com>
2022-08-23 15:08:08 -0400

    commented todo Model

--- models/Todo.js
+++ models/Todo.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose') // require mongoose which allows for the schema
 
-const TodoSchema = new mongoose.Schema({
-  todo: {
-    type: String,
-    required: true,
+const TodoSchema = new mongoose.Schema({ // make a constructor called TodoSchema it is a new mongoose schema
+  todo: { // first feild (key) called todo
+    type: String, // its type is a string
+    required: true, // and it is required
   },
-  completed: {
-    type: Boolean,
-    required: true,
+  completed: { // second feild (key)) called completed
+    type: Boolean, // type (of 8 possible) is boolean (truthy/falsy)
+    required: true, // and it is required
   }
 })
 
-module.exports = mongoose.model('Todo', TodoSchema)
+module.exports = mongoose.model('Todo', TodoSchema) // export this as Todo and TodoSchema for use by other files
